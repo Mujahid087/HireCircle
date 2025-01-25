92893ad192ff6d74bcdf57775fb5f0f7064b92d9
Mujahid087 <mujahidmahedi10@gmail.com>
2025-01-25 16:36:50 +0530

    creating the frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+function App() {
+  return (
+    <>
+      <div className="flex justify-center items-center h-screen bg-gray-100">
+        <button className="inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors focus:outline-none focus:ring-2 focus:ring-blue-500 disabled:pointer-events-none disabled:opacity-50 bg-blue-600 text-white shadow hover:bg-blue-700 h-9 px-4 py-2">
+          Button
+        </button>
+      </div>
+
+    </>
+  );
+}
+
+export default App;
