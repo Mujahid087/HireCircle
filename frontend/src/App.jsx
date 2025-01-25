2238dd4cdeaf06e5cb0ce52c6b6aae5894885d89
Mujahid087 <mujahidmahedi10@gmail.com>
2025-01-25 22:09:59 +0530

    creating new folder

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,10 @@
-import React from 'react';
-
-
-function App() {
+const App = () => {
   return (
-    <>
-      <div className="flex justify-center items-center h-screen bg-gray-100">
-        <button className="inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors focus:outline-none focus:ring-2 focus:ring-blue-500 disabled:pointer-events-none disabled:opacity-50 bg-blue-600 text-white shadow hover:bg-blue-700 h-9 px-4 py-2">
-          Button
-        </button>
-      </div>
-
-    </>
+    <div className="bg-blue-500 p-5 text-white text-center">
+      <h1 className="text-3xl">Hello, Tailwind CSS!</h1>
+    </div>
   );
-}
+};
 
 export default App;
+
