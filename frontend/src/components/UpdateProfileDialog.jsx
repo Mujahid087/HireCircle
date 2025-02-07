e1b2189418e096b0bdb4289a01bbcb5d6cc7b20a
Mujahid087 <mujahidmahedi10@gmail.com>
2025-02-07 22:48:01 +0530

    working on updating the profile

--- frontend/src/components/UpdateProfileDialog.jsx
+++ frontend/src/components/UpdateProfileDialog.jsx
@@ -1,35 +1,70 @@
 import React, { useState } from 'react'
-import { Dialog, DialogFooter, DialogHeader } from './ui/dialog'
-import { DialogContent, DialogTitle } from './ui/dialog'
+import { Dialog, DialogContent, DialogFooter, DialogHeader, DialogTitle } from './ui/dialog'
 import { Label } from './ui/label'
 import { Input } from './ui/input'
 import { Button } from './ui/button'
 import { Loader2 } from 'lucide-react'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import axios from 'axios'
+import { USER_API_END_POINT } from '../../utils/constant.js'
+import { setUser } from '@/redux/authSlice'
+import { toast } from 'sonner'
 
 const UpdateProfileDialog = ({ open, setOpen }) => {
     const [loading, setLoading] = useState(false);
-    const {user}=useSelector(store=>store.auth)
+    const { user } = useSelector(store => store.auth);
 
-    const [input,setInput]=useState({
-        fullname:user?.fullname,
-        email:user?.email,
-        phoneNumber:user?.phoneNumber,
-        bio:user?.profile?.bio,
-        skills:user?.profile?.skills?.map(skill=>skill),
-        file:user?.profile?.resume
-    })
+    const [input, setInput] = useState({
+        fullname: user?.fullname || "",
+        email: user?.email || "",
+        phoneNumber: user?.phoneNumber || "",
+        bio: user?.profile?.bio || "",
+        skills: user?.profile?.skills?.map(skill => skill) || "",
+        file: user?.profile?.resume || ""
+    });
+    const dispatch = useDispatch();
 
-    const changeEventHandler=(e)=>{
-        setInput({...input,[e.target.name]:e.target.value})
+    const changeEventHandler = (e) => {
+        setInput({ ...input, [e.target.name]: e.target.value });
     }
-    const submitHandler =(e)=>{
+
+    const fileChangeHandler = (e) => {
+        const file = e.target.files?.[0];
+        setInput({ ...input, file })
+    }
+
+    const submitHandler = async (e) => {
         e.preventDefault();
-        console.log(input)
+        const formData = new FormData();
+        formData.append("fullname", input.fullname);
+        formData.append("email", input.email);
+        formData.append("phoneNumber", input.phoneNumber);
+        formData.append("bio", input.bio);
+        formData.append("skills", input.skills);
+        if (input.file) {
+            formData.append("file", input.file);
+        }
+        try {
+            setLoading(true);
+            const res = await axios.post(`${USER_API_END_POINT}/profile/update`, formData, {
+                headers: {
+                    'Content-Type': 'multipart/form-data'
+                },
+                withCredentials: true
+            });
+            if (res.data.success) {
+                dispatch(setUser(res.data.user));
+                toast.success(res.data.message);
+            }
+        } catch (error) {
+            console.log(error);
+            toast.error(error.response.data.message);
+        } finally{
+            setLoading(false);
+        }
+        setOpen(false);
+        console.log(input);
     }
-    
-
-    
 
 
 
@@ -38,9 +73,7 @@ const UpdateProfileDialog = ({ open, setOpen }) => {
             <Dialog open={open}>
                 <DialogContent className="sm:max-w-[425px]" onInteractOutside={() => setOpen(false)}>
                     <DialogHeader>
-                        <DialogTitle>
-                            Update Profile
-                        </DialogTitle>
+                        <DialogTitle>Update Profile</DialogTitle>
                     </DialogHeader>
                     <form onSubmit={submitHandler}>
                         <div className='grid gap-4 py-4'>
@@ -71,7 +104,6 @@ const UpdateProfileDialog = ({ open, setOpen }) => {
                                 <Input
                                     id="number"
                                     name="number"
-                                    type="number"
                                     value={input.phoneNumber}
                                     onChange={changeEventHandler}
                                     className="col-span-3"
@@ -104,16 +136,15 @@ const UpdateProfileDialog = ({ open, setOpen }) => {
                                     name="file"
                                     type="file"
                                     accept="application/pdf"
+                                    onChange={fileChangeHandler}
                                     className="col-span-3"
                                 />
                             </div>
-
                         </div>
                         <DialogFooter>
                             {
-                                loading ? <Button className="w-full my-4"><Loader2 className='mr-2 h-4 w-4 animate-spin' />please wait</Button> : <Button type="submit" className="w-full my-4">Update</Button>
+                                loading ? <Button className="w-full my-4"> <Loader2 className='mr-2 h-4 w-4 animate-spin' /> Please wait </Button> : <Button type="submit" className="w-full my-4">Update</Button>
                             }
-
                         </DialogFooter>
                     </form>
                 </DialogContent>
